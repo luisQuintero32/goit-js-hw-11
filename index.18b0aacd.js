dd1933dfe1541872ab99f897df5e1d108833b539
luisQuintero32 <115516045+luisQuintero32@users.noreply.github.com>
2023-05-06 03:10:54 +0000

    Deploying to gh-pages from @ luisQuintero32/goit-js-hw-11@52a0ff35be9611f50f26ee399837180b7656e1d0 🚀

---
+++ index.18b0aacd.js
@@ -0,0 +1,2 @@
+var loader=document.querySelector(".loader_page"),timerId=null;function hidenItem(){console.log("hello"),timerId=setInterval((function(){loader.classList.add("loadhide")}),2400)}hidenItem();
+//# sourceMappingURL=index.18b0aacd.js.map
---
+++ index.2e57678a.js
@@ -0,0 +1,2 @@
+var darkMode=localStorage.getItem("darkMode"),darkModeToggle=document.querySelector("#Darkbutton"),enableDarkMode=function(){document.body.classList.add("darkmode"),localStorage.setItem("darkMode","enabled")},disableDarkMode=function(){document.body.classList.remove("darkmode"),localStorage.setItem("darkMode","disabled")};"enabled"===darkMode&&enableDarkMode(),darkModeToggle.addEventListener("click",(function(){"enabled"!==(darkMode=localStorage.getItem("darkMode"))?enableDarkMode():disableDarkMode()}));
+//# sourceMappingURL=index.2e57678a.js.map
